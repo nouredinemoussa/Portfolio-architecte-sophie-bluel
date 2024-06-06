262255a91af54c208c4465837293d1b2d00d3c9a
nouredinemoussa <nouredine.moussa1@gmail.com>
2024-06-06 12:27:18 +0200

    récupération et ajout des travaux

---
+++ FrontEnd/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Fonction pour récupérer les travaux
+    function fetchWorks() {
+      fetch("http://localhost:5678/api/works")
+        .then(response => response.json())
+        .then(data => {
+          addWorks(data);
+        })
+        .catch(error => {
+          console.error('Erreur:', error);
+        });
+    }
+  
+
+    function addWorks(works) {
+      const workGallery = document.querySelector(".gallery");
+      works.forEach(work => {
+        const workFigure = document.createElement("figure");
+        workFigure.id = `work-${work.id}`;
+  
+        const workImg = document.createElement("img");
+        workImg.src = work.imageUrl;
+        workImg.alt = work.title;
+  
+        const workFigCaption = document.createElement("figcaption");
+        workFigCaption.textContent = work.title;
+  
+        workFigure.appendChild(workImg);
+        workFigure.appendChild(workFigCaption);
+  
+        workGallery.appendChild(workFigure);
+      });
+    }
+  
+ 
+    fetchWorks();
+  });
+  
