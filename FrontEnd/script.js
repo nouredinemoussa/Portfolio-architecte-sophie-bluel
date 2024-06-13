747de22f2ceee82668e5bb5b2ac446205b363382
nouredinemoussa <nouredine.moussa1@gmail.com>
2024-06-13 15:31:52 +0200

    Ajout des catégories et filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  let worksData = [];
     function fetchWorks() {
       fetch("http://localhost:5678/api/works")
         .then(response => response.json())
         .then(data => {
-          addWorks(data);
+          worksData = data;
+          addWorks(worksData);
         })
-        .catch(error => {
-          console.error('Erreur:', error);
-        });
     }
   
 
     function addWorks(works) {
       const workGallery = document.querySelector(".gallery");
+      workGallery.innerHTML = '';
       works.forEach(work => {
         const workFigure = document.createElement("figure");
         workFigure.id = `work-${work.id}`;
@@ -32,6 +32,42 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
  
-    fetchWorks();
+    
+     function fetchCategories() {
+     fetch("http://localhost:5678/api/categories")
+     .then(response => response.json())
+     .then(data => {
+      addCategories(data);
+    })
+  }
+
+     function addCategories(categories) {
+      const categoriesGroup = document.querySelector(".categories")
+
+      const buttonTous = document.createElement("button");
+      buttonTous.textContent = "Tous";
+      buttonTous.className = "filter";
+      buttonTous.addEventListener("click", () =>
+      {
+        addWorks(worksData);
+      });
+      categoriesGroup.appendChild(buttonTous);
+
+      categories.forEach(category => {
+        const buttonCategory = document.createElement("button");
+        buttonCategory.textContent = category.name;
+        buttonCategory.addEventListener("click", () => {
+          categoryFilter(category.id);
+        });
+        buttonCategory.className = "filter";
+        categoriesGroup.appendChild(buttonCategory);
+      })
+     }
+     function categoryFilter(categoryId) {
+      const filteredWorks = worksData.filter(work => work.categoryId === categoryId);
+      addWorks(filteredWorks);
+     }
+     fetchCategories();
+     fetchWorks();
   });
-  
+ 
